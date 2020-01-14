13b36a56a79a29cb5146834f0a331107bb6535fe
Sowren Sen <sowrensen@gmail.com>
2020-01-14 18:32:20 +0600

    Fixed project root bug.

--- fileculator.py
+++ fileculator.py
@@ -1,19 +1,19 @@
-#! /usr/bin/python3
-
-# ------------------------------------------------------------
-# fileculator.py
-# ------------------------------------------------------------
-# This scripts calculates the total size of the 'storage/app'
-# directory (user uploaded files) of multiple Laravel apps. 
-# Define the project root and depth in .env file and run 
-# the script as a cron job.
-#
-# Author: Sowren Sen
-
+#!/usr/bin/env python3
+"""
+| ------------------------------------------------------------
+| fileculator.py
+| ------------------------------------------------------------
+| This scripts calculates the total size of the 'storage/app'
+| directory (user uploaded files) of multiple Laravel apps. 
+| Define the project root and depth in .env file and run 
+| the script as a cron job.
+|
+"""
 import os
 from pathlib import Path
 from datetime import datetime
 import json
+import sys
 
 
 def calculate_size(project_root, project_depth):
@@ -21,12 +21,18 @@ def calculate_size(project_root, project_depth):
     Calculate total size of user uploaded files in storage/app directory and
     write the size information in a json file inside the public directory.
     """
-    # Get the absolute path
-    ROOT_DIRECTORY = Path.joinpath(Path.home(), project_root)
-    
+    # Validate project directory
+    try:
+        ROOT_DIRECTORY = Path(project_root)
+        if not os.path.exists(ROOT_DIRECTORY):
+            raise IOError('Error! Project directory not found, aborting...')
+    except IOError as error:
+        print(str(error))
+        sys.exit(0)
+
     # The glob for searching
     TARGET_LARAVEL_DIRECTORY = '*' * int(project_depth) + '/*storage/app'
-    
+
     # Search and get the Laravel projects, in this case, we are
     # targeting directories which has /storage/app subdirectory,
     # i.e. where user uploaded files reside.
@@ -41,7 +47,8 @@ def calculate_size(project_root, project_depth):
         total_size = 0
         total_size = sum(
             file.stat().st_size for file in public_directory.glob('**/*') if file.is_file())  # Total file size in bytes
-        print('%s has %s of files' % (public_directory, human_readable_size(total_size)))
+        print('%s has %s of files' %
+              (public_directory, human_readable_size(total_size)))
         # Json file path, we'll be keeping it inside storage/app, since
         # this file is not and should not be included in any vcs.
         path = public_directory.joinpath('storage.json')
@@ -83,17 +90,29 @@ def run():
     """
     try:
         valid_depths = (1, 2)
-        print('Reading environment variables from .env file...\n')
+        print('Reading environment variables from .env file...')
         project_root = os.environ['PROJECT_ROOT']
         project_depth = os.environ['PROJECT_DEPTH']
+
+        if not project_root:
+            raise ValueError(
+                "Error! Project root is not defined. You must set a project root in .env file.")
+
         if int(project_depth) not in valid_depths:
-            raise ValueError('Error! Invalid value defined for depth, value must be between 1 and 2.')
+            raise ValueError(
+                'Error! Invalid value defined for depth, value must be between 1 and 2.')
+
+        print("""
+        Project Root:           %s
+        Project Depth:          %s
+        """ % (project_root, project_depth))
+
         calculate_size(project_root, project_depth)
         return
     except ValueError as error:
         print(error)
     except KeyError as error:
-        print('Error! Please define necessary keys in .env file.')
+        print('\nError! Please define necessary keys in .env file.')
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -7,19 +7,42 @@ the script as a cron job.
 
 Changelog
 ------
+
+#### Version 1.0.1
+
+ - Fixed bug related to project root.
+
 #### Version 1.0
+
  - Initial release
 
 Usage
 ------
+
+### Setup
+
+Run following commands in your terminal to copy this repository and for primary setup. 
+You will need [Pipenv](https://github.com/pypa/pipenv) to use this script however.
+
+```
+git clone https://github.com/sowrensen/fileculator.git
+cd fileculator
+pipenv install
+cp .env.example .env
+```
+
 ### Enviroment Variables
+
 Before you can run the script, you've to declare two environment variables 
-in `.env` file. I have added an example file for your consistency. Just copy
-the `.env.example` file as `.env` and define the values for the following two 
-keys.
- - **PROJECT_ROOT**: This will be the directory where all of your Laravel
- projects reside. Note that, if you keep your projects into some place like
- `/var/www/`, you have to run the script as root.
+in `.env` file. An example `.env` file has been added for your consistency. 
+Just copy the `.env.example` file as `.env` and define the values for the 
+following two keys.
+
+ - **PROJECT_ROOT**: This will be the directory where all of your Laravel 
+  projects reside, e.g.` /home/<user>/project`s, or `/var/www`, or 
+  `/usr/share/nginx`. Note that, if you keep your projects into 
+  some place which is in root directory, you have to run the 
+  script as root. You know how Linux works, right?
  
  - **PROJECT_DEPTH**: This value defines the searching depths for directories.
  The value should be either 1 or 2. If you have subdirectories in your root 
@@ -34,12 +57,14 @@ keys.
     - App_2_Instance_1/
     - App_2_Instance_2/
    ``` 
+
    In the above case, you have to put depth value 2. If there is only one
    level of application directory, you can put 1.
 
 
 Output
 ------
+
 The script calculates the total size (in bytes) of the `/storage/app` directory 
 inside each Laravel project and puts the storage information into a json file 
 named `storage.json` inside that directory which is Laravel's public directory. 
@@ -54,21 +79,30 @@ Thus the generated file is ignored in any version control system. The data insid
 ```
 
 Laravel can use the information for various purposes. Since this is an expensive operation, 
-you should run the script as a cron job so that it doesn't interfere with HTTP response.
+you should run the script as a cron job so that it doesn't interfere with HTTP response. 
+_Note that, the output file will be replaced during the next run_.
 
 Cron Example
 ------------
 
-You can run the file directly from cron job. However, if you face difficulties to run pipenv from cron, better you create a bash script like this.
+You can run the file directly from cron job. However, if you face difficulties to run 
+pipenv from cron, better you create a bash script like this.
+
 ```
 #!/bin/sh
 
-VENV_PYTHON="/path/to/pipenv/python"
+PYTHON="/path/to/pipenv/python"
 FILECULATOR="/path/to/fileculator"
 PIPENV="/path/to/pipenv"
 SCRIPT="fileculator.py"
 
-cd "${FILECULATOR}" && "${PIPENV}" run "${VENV_PYTHON}" "${SCRIPT}"
+cd "${FILECULATOR}" && "${PIPENV}" run "${PYTHON}" "${SCRIPT}"
+```
+
+Make the bash script executable by running:
+
+```shell
+chmod +x fileculator.sh
 ```
 
 Now run the bash script as cron job. This following cron job will run on every even hour and write the log into the defined log file.
